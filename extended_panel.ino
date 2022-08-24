d00c814414c469b80e33e903d298a15b5c72182c
Ryan Lett <ryan.james.lett@gmail.com>
2022-08-23 18:41:19 -0700

    Compiling

--- extended_panel.ino
+++ extended_panel.ino
@@ -1,9 +1,54 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_NeoMatrix.h>
+#include <Adafruit_NeoPixel.h>
+#include "panel.h"
+
+#define BRIGHTNESS 30
+#define DELAY 10
+#define INIT_VALUE 125
+
+#define GRID_PIN 3
+#define GRID_HEIGHT 8
+#define GRID_LENGTH 32
+#define TOP_STRIP_PIN 5
+#define BOT_STRIP_PIN 6
+#define STRIP_LENGTH 58
+
+
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(
+    GRID_LENGTH,
+    GRID_HEIGHT,
+    GRID_PIN,
+    NEO_MATRIX_RIGHT   + NEO_MATRIX_BOTTOM +
+    NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG,
+    NEO_GRB            + NEO_KHZ800
+);
+
+Adafruit_NeoPixel top_strip = Adafruit_NeoPixel(
+    STRIP_LENGTH,
+    TOP_STRIP_PIN,
+    NEO_GRB + NEO_KHZ800
+);
+
+Adafruit_NeoPixel bot_strip = Adafruit_NeoPixel(
+    STRIP_LENGTH,
+    BOT_STRIP_PIN,
+    NEO_GRB + NEO_KHZ800
+);
+
+Panel panel = Panel(
+    &matrix,
+    &top_strip,
+    &bot_strip
+    );
+
 void setup() {
   // put your setup code here, to run once:
-
+    panel.init(BRIGHTNESS, INIT_VALUE);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  panel.show();
+  delay(DELAY);
 }
--- panel.h
+++ panel.h
@@ -8,39 +8,45 @@ class Panel
     private:
         Adafruit_NeoMatrix *_matrix;
         Adafruit_NeoPixel *_top_strand, *_bottom_strand;
+        int _brightness, _init_value;
 
     public:
-        Panel();
-        Panel(Adafruit_NeoMatrix *matrix, Adafruit_NeoPixel *top, Adafruit_NeoPixel *bottom);
+        Panel(Adafruit_NeoMatrix *matrix,
+              Adafruit_NeoPixel *top,
+              Adafruit_NeoPixel *bottom
+        );
+
+        void init(int brightness, int init_value);
         void show();
-}
+};
 
-/**
- * Empty Constructor
- */
-Panel::Panel() {}
-
-/**
- * Construct a panel
- */
 Panel::Panel(
         Adafruit_NeoMatrix *matrix,
         Adafruit_NeoPixel *top,
-        Adafruit_NeoPixel *bottom,
-        int _brightness
+        Adafruit_NeoPixel *bottom
 ) {
-    *_matrix = *matrix;
+    _matrix = matrix;
     *_top_strand = *top;
     *_bottom_strand = *bottom;
+}
+
+void Panel::init(int brightness, int init_value) {
     _brightness = brightness;
+    _init_value = init_value;
+
+    _matrix->begin();
+    _matrix->setBrightness(_brightness);
+    _top_strand->begin();
+    _top_strand->setBrightness(_brightness);
+    _bottom_strand->begin();
+    _bottom_strand->setBrightness(_brightness);
+    _matrix->fillScreen(_init_value);
+    _top_strand->fill(_init_value);
+    _bottom_strand->fill(_init_value);
 }
 
 void Panel::show() {
-    _matrix.begin();
-    _matrix.setBrightness(_brightness);
-    _top_strand.begin();
-    _top_strand.setBrightness(_brightness);
-    _bottom_strand.begin();
-    _bottom_strand.setBrightness(_brightness);
+  _matrix->show();
+  _top_strand->show();
+  _bottom_strand->show();
 }
-
